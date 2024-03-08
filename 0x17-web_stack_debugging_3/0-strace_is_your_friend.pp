d2dc8df61cb179428cab6e2886c91901db18945f
mikolanny <olaniyanlanny@gmail.com>
2024-03-07 18:36:02 -0800

    [added]: file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# Fix the wrong file name in /var/www/html/wp-settings.php
+# Rename the file from .phpp to .php
+
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
